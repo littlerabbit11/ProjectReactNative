b2f2f43e94e50d0243bd7d77ce54e77ddfedc23c
Siwakorn Danglerd <62067567+littlerabbit11@users.noreply.github.com>
2021-12-22 12:01:40 +0700

    WebView and Stack Navigation

---
+++ 12-22-2021/AppV3.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import WebViewExample from './components/WebViewExample';
+import WebViewLoading from './components/WebViewLoading';
+
+const App = () => {
+  return (
+    //<WebViewExample/>
+    <WebViewLoading />
+  );
+};
+
+export default App;
