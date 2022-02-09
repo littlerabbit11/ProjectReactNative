01a3a838435c5361f8053cd0663d3007b8197f0f
Siwakorn Danglerd <62067567+littlerabbit11@users.noreply.github.com>
2022-02-09 12:45:56 +0700

    9/2/22 Part 2

--- 02-09-2022/part 2/screens/ProductScreen.js
+++ 02-09-2022/part 2/screens/ProductScreen.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, ActivityIndicator, FlatList } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import {
   HeaderButtons,
   HeaderButton,
   Item,
   HiddenItem,
   OverflowMenu,
-} from 'react-navigation-header-buttons';
+} from "react-navigation-header-buttons";
+import axios from "axios";
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+  Badge,
+} from "native-base";
 
-const IoniconsHeaderButton = props => (
+const IoniconsHeaderButton = (props) => (
   // the `props` here come from <Item ... />
   // you may access them and pass something else to `HeaderButton` if you like
   <HeaderButton IconComponent={Ionicons} iconSize={23} {...props} />
 );
 
-const ProductScreen = ({navigation}) => {
+const ProductScreen = ({ navigation }) => {
   React.useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -23,7 +38,7 @@ const ProductScreen = ({navigation}) => {
           <Item
             title="Register"
             iconName="person-add"
-            onPress={() => alert('Registering')}
+            onPress={() => navigation.navigate("Register")}
           />
         </HeaderButtons>
       ),
@@ -39,15 +54,51 @@ const ProductScreen = ({navigation}) => {
     });
   }, [navigation]);
 
+  const [product, setProduct] = useState([]);
+  //useEffect when A Product is selected
+  useEffect(() => {
+    //getData from server
+    const getData = async () => {
+      const res = await axios.get("https://api.codingthailand.com/api/course");
+      // alert(JSON.stringify(res.data.data[0].title));
+      setProduct(res.data.data);
+    };
+    getData();
+
+    // return () => {
+    //   second;
+    // };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Ionicons name="home" size={50} color="skyblue" />
-      <Text>Product page</Text>
-      <Button
-        title="Go to About Me"
-        onPress={() =>
-          navigation.navigate('About', {email: 'da.siwakorn_st@tni.ac.th'})
-        }
+    <View>
+      <FlatList
+        // set the data form server
+        data={product}
+        // Extract the key from the data with keyExtractor
+        keyExtractor={(item, index) => item.id.toString()}
+        // render the data with renderItem
+        renderItem={({ item }) => (
+          <ListItem thumbnail>
+            <Left>
+              <Thumbnail square source={{ uri: item.picture }} />
+            </Left>
+            <Body>
+              <Text>{item.title}</Text>
+              <Text note numberOfLines={1}>
+                {item.detail}
+              </Text>
+            </Body>
+            <Right>
+              <Badge danger>
+                <Text>{item.view}</Text>
+              </Badge>
+              {/* <Button transparent>
+                <Text>View</Text>
+              </Button> */}
+            </Right>
+          </ListItem>
+        )}
       />
     </View>
   );
@@ -58,7 +109,7 @@ export default ProductScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
